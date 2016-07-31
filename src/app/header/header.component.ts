0c4c9ed92dd1d0a2f7598932a9237d82008fabfe
Will <doggy.huang@gmail.com>
2016-07-31 10:43:16 +0800

    建立 header 元件，並且將程式碼與範本搬進去

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-header',
+  templateUrl: 'header.component.html',
+  styleUrls: ['header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  title = 'The Will Will Web';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
