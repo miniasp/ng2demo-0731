231c57dcdca5f8473a792d3cf7478bc723ef8580
Will <doggy.huang@gmail.com>
2016-07-31 17:24:53 +0800

    改用 HTTP_PROVIDERS 取得 API 的資料

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
 import { SearchService } from './app/search.service';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent, [SearchService]);
+bootstrap(AppComponent, [SearchService, HTTP_PROVIDERS]);
